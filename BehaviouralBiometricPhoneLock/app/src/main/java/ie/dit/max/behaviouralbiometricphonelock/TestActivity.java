73eb414ca907ba5b1d276f835cd393c0eb5deac6
MaximilianMihoc <maximilian.mihoc@mydit.ie>
2015-11-07 16:22:16 +0000

    added angular velocity and linear acceleration in test activity

--- BehaviouralBiometricPhoneLock/app/src/main/java/ie/dit/max/behaviouralbiometricphonelock/TestActivity.java
+++ BehaviouralBiometricPhoneLock/app/src/main/java/ie/dit/max/behaviouralbiometricphonelock/TestActivity.java
@@ -1,6 +1,11 @@
 package ie.dit.max.behaviouralbiometricphonelock;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -23,7 +28,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TestActivity extends Activity implements
-        GestureDetector.OnGestureListener
+        GestureDetector.OnGestureListener,
+        SensorEventListener
 {
     //Test if OpenCV initialisation was successful or it failed
     static {
@@ -36,6 +42,16 @@ public class TestActivity extends Activity implements
 
     private static final String DEBUG_TAG = "Gestures";
     private GestureDetectorCompat mDetector;
+
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+    private Sensor senGyroscope;
+
+    private float lastLinearAcceleration;
+    private float linearAcceleration;
+
+    private float lastAngularVelocity;
+    private float angularVelocity;
 
     Point startPoint, endPoint;
     ArrayList<Point> points = new ArrayList<Point>();
@@ -57,6 +73,22 @@ public class TestActivity extends Activity implements
 
         outputdata = (TextView) findViewById(R.id.predictions);
 
+        linearAcceleration = 0.0f;
+        lastLinearAcceleration = 0.0f;
+
+        angularVelocity = 0.0f;
+        lastAngularVelocity = 0.0f;
+
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        // Accelerometer declarations
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+
+        // Gyroscope declarations
+        senGyroscope = senSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        senSensorManager.registerListener(this, senGyroscope, SensorManager.SENSOR_DELAY_FASTEST);
+
         //initialise svm.
         svm =  SVM.create();
         svm.setKernel(SVM.INTER);
@@ -72,7 +104,7 @@ public class TestActivity extends Activity implements
         Bundle bundle = getIntent().getExtras();
         trainObservations = (ArrayList<Observation>) bundle.getSerializable("trainObservations");
 
-        Mat trainMat = new Mat(trainObservations.size(), 8, CvType.CV_32FC1);
+        Mat trainMat = new Mat(trainObservations.size(), 10, CvType.CV_32FC1);
         Mat labelsMat = new Mat(trainObservations.size(), 1, CvType.CV_32S);
 
         for(int i = 0; i < trainObservations.size(); i++)
@@ -88,6 +120,10 @@ public class TestActivity extends Activity implements
             // linear accelerations are part of the observation
             trainMat.put(i, 6, trainObservations.get(i).getLastLinearAcceleration());
             trainMat.put(i, 7, trainObservations.get(i).getLinearAcceleration());
+
+            // angular Velocity are part of the observation
+            trainMat.put(i, 8, trainObservations.get(i).getLastAngularVelocity());
+            trainMat.put(i, 9, trainObservations.get(i).getAngularVelocity());
 
             // all labels are 1 in training scenario. I presume that the owner is performing in the training area.
             labelsMat.put(i, 0, 1);
@@ -173,10 +209,20 @@ public class TestActivity extends Activity implements
                     tempObs.setGesture(tap);
                 }
 
+                // add linear accelerations to the Observation
+                tempObs.setLinearAcceleration(linearAcceleration);
+                tempObs.setLastLinearAcceleration(lastLinearAcceleration);
+                Log.d(DEBUG_TAG, "Linear Accelerations on touch - lastLinearAcceleration: " + lastLinearAcceleration + " LinearAcceleration: " + linearAcceleration);
+
+                // add angular velocity to the Observation on touch gesture
+                tempObs.setAngularVelocity(angularVelocity);
+                tempObs.setLastAngularVelocity(lastAngularVelocity);
+                Log.d(DEBUG_TAG, "Angular Velocity on touch - lastAngularVelocity: " + lastAngularVelocity + " Angular Velocity: " + angularVelocity);
+
                 // add observation to testList to remember what observations we tested so far
                 testObservations.add(tempObs);
 
-                Mat testDataMat = new Mat(testObservations.size(), 8, CvType.CV_32FC1);
+                Mat testDataMat = new Mat(testObservations.size(), 10, CvType.CV_32FC1);
                 //write code to predict one Observation at a time. This can be later written in a separate function
                 for(int i = 0; i < testObservations.size(); i++)
                 {
@@ -191,6 +237,10 @@ public class TestActivity extends Activity implements
                     // get linear accelerations
                     testDataMat.put(i, 6, testObservations.get(i).getLastLinearAcceleration());
                     testDataMat.put(i, 7, testObservations.get(i).getLinearAcceleration());
+
+                    // get angular velocity
+                    testDataMat.put(i, 8, testObservations.get(i).getLastAngularVelocity());
+                    testDataMat.put(i, 9, testObservations.get(i).getAngularVelocity());
 
                 }
                 // create the result Mat
@@ -252,5 +302,63 @@ public class TestActivity extends Activity implements
     {
         isFling = true;
         return true;
+    }
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent)
+    {
+        Sensor mySensor = sensorEvent.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_GYROSCOPE)
+        {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            lastAngularVelocity = angularVelocity;
+            angularVelocity = (float) Math.sqrt((double) (x*x + y*y + z*z));
+
+            //Log.d(DEBUG_TAG, "Sensor - lastAngularVelocity: " + lastAngularVelocity + " angularVelocity: " + angularVelocity);
+
+            //Log.i("Gyroscope ", " x= " + x + " y= " + y + " z= " + z);
+        }
+
+        if (mySensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION)
+        {
+            //this sensor gives me the linear acceleration values for x, y, z.
+            // linear acceleration is the acceleration - earth gravity.
+
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            lastLinearAcceleration = linearAcceleration;
+            linearAcceleration = (float) Math.sqrt((double) (x*x + y*y + z*z));
+
+            //Log.d(DEBUG_TAG, "Sensor - lastLinearAcceleration: " + lastLinearAcceleration + " LinearAcceleration: " + linearAcceleration);
+
+            //Log.i("Accelerometer", " x= " + x + " y= " + y + " z= " + z);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy)
+    {
+
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+        senSensorManager.registerListener(this, senGyroscope, SensorManager.SENSOR_DELAY_FASTEST);
+
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        senSensorManager.unregisterListener(this);
     }
 }
