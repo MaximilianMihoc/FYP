e14a7bd5b7a0277e1b1c052d316b1dfa44f1e4c8
MaximilianMihoc <maximilian.mihoc@mydit.ie>
2016-03-25 18:05:16 +0000

    few changes

--- BehaviouralBiometricPhoneLock/app/src/main/java/ie/dit/max/behaviouralbiometricphonelock/MyLockScreenActivity.java
+++ BehaviouralBiometricPhoneLock/app/src/main/java/ie/dit/max/behaviouralbiometricphonelock/MyLockScreenActivity.java
@@ -19,7 +19,7 @@ import android.widget.Toast;
 public class MyLockScreenActivity extends Activity implements View.OnClickListener
 {
     private static final int ADMIN_INTENT = 15;
-    private static final String description = "Sample Administrator description";
+    private static final String description = "";
     private DevicePolicyManager mDevicePolicyManager;
     private ComponentName mComponentName;
 
@@ -32,10 +32,8 @@ public class MyLockScreenActivity extends Activity implements View.OnClickListen
         mComponentName = new ComponentName(this, MyAdminReceiver.class);
         Button btnEnableAdmin = (Button) findViewById(R.id.btnEnable);
         Button btnDisableAdmin = (Button) findViewById(R.id.btnDisable);
-        Button btnLock = (Button) findViewById(R.id.btnLock);
         btnEnableAdmin.setOnClickListener(this);
         btnDisableAdmin.setOnClickListener(this);
-        btnLock.setOnClickListener(this);
     }
 
     @Override
@@ -51,15 +49,6 @@ public class MyLockScreenActivity extends Activity implements View.OnClickListen
             case R.id.btnDisable:
                 mDevicePolicyManager.removeActiveAdmin(mComponentName);
                 Toast.makeText(getApplicationContext(), "Admin registration removed", Toast.LENGTH_SHORT).show();
-                break;
-
-            case R.id.btnLock:
-                boolean isAdmin = mDevicePolicyManager.isAdminActive(mComponentName);
-                if (isAdmin) {
-                    mDevicePolicyManager.lockNow();
-                }else{
-                    Toast.makeText(getApplicationContext(), "Not Registered as admin", Toast.LENGTH_SHORT).show();
-                }
                 break;
         }
     }
