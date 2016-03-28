d87f156fc0d9552067a91d1e765b33343ec04bf6
MaximilianMihoc <maximilian.mihoc@mydit.ie>
2016-03-28 14:45:51 +0100

    Delete Menu files

--- BehaviouralBiometricPhoneLock/app/src/main/java/ie/dit/max/foregroundAppStackOverflow/QuestionBodyScreen.java
+++ BehaviouralBiometricPhoneLock/app/src/main/java/ie/dit/max/foregroundAppStackOverflow/QuestionBodyScreen.java
@@ -75,29 +75,4 @@ public class QuestionBodyScreen extends TestBehaviouralBiometrics
         });
 
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_question_body_screen, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the StackOverflowHomeScreen/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
-        {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
